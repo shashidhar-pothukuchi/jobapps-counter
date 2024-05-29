efa82b5e9907ee9fb268537ef4c6d98d0f880352
Sai Durga Shashidhar <saithechinnu@gmail.com>
2024-05-29 11:33:32 -0400

    patch adder

--- background.js
+++ background.js
@@ -1,25 +1,10 @@
-addCount = () => {
-  //   localStorage.setItem("counter", value);
+chrome.runtime.onInstalled.addListener(() => {
+  dailyReset();
+});
 
-  chrome.storage.local.get(["applied", "totalJobs"], async (res) => {
-    console.log("Background", res.applied);
-    chrome.storage.local.set({ applied: res.applied + 1 });
-    chrome.storage.local.set({ totalJobs: res.totalJobs + 1 });
-  });
-};
-
-deleteCount = () => {
-  chrome.storage.local.get(["applied", "totalJobs"], async (res) => {
-    console.log("Background", res.applied);
-    if (res.applied > 0) {
-      chrome.storage.local.set({
-        totalJobs: res.totalJobs - 1 >= 0 ? res.totalJobs - 1 : 0,
-      });
-      let cur = res.applied - 1 >= 0 ? res.applied - 1 : 0;
-      chrome.storage.local.set({ applied: cur });
-    }
-  });
-};
+chrome.runtime.onStartup.addListener(() => {
+  dailyReset();
+});
 
 chrome.commands.onCommand.addListener((command) => {
   if (command === "increment") {
@@ -31,3 +16,54 @@ chrome.commands.onCommand.addListener((command) => {
     deleteCounter();
   }
 });
+
+// Data Structure for cdate
+// cdate:{date: date of the transation, applied: Jobs applied on that day}
+
+dailyReset = () => {
+  chrome.storage.local.get(["history", "cdate"], (res) => {
+    const today = new Date().toLocaleDateString();
+
+    if (res.cdate && res.cdate.date !== today) {
+      chrome.storage.local.set(
+        { cdate: { date: today, applied: 0 } },
+        async () => {
+          let history = res.history || [];
+          history.push({ date: res.cdate.date, applied: res.cdate.applied });
+          chrome.storage.local.set({ history });
+          console.log("Initiated date with value", res.cdate);
+        }
+      );
+    }
+    if (!res.cdate) {
+      chrome.storage.local.set({ cdate: { date: today, applied: 0 } });
+    }
+  });
+};
+
+addCount = () => {
+  //   localStorage.setItem("counter", value);
+
+  chrome.storage.local.get(["cdate", "totalJobs"], async (res) => {
+    console.log("Background", res.cdate.applied);
+    chrome.storage.local.set({
+      cdate: { date: res.cdate.date, applied: res.cdate.applied + 1 },
+    });
+    chrome.storage.local.set({ totalJobs: res.totalJobs + 1 });
+  });
+};
+
+deleteCount = () => {
+  chrome.storage.local.get(["cdate", "totalJobs"], async (res) => {
+    console.log("Background", res.cdate.applied);
+    if (res.cdate.applied > 0) {
+      chrome.storage.local.set({
+        totalJobs: res.totalJobs - 1 >= 0 ? res.totalJobs - 1 : 0,
+      });
+      let cur = res.applied - 1 >= 0 ? res.applied - 1 : 0;
+      chrome.storage.local.set({
+        cdate: { date: res.cdate.date, applied: cur },
+      });
+    }
+  });
+};
